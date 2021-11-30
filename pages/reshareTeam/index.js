40b20313c66a3fb31cb6946cbf93dd05ffab945a
Daniel Fu <danielfu123@gmail.com>
2021-11-30 15:12:20 -0800

    test

--- pages/reshareTeam/index.js
+++ pages/reshareTeam/index.js
@@ -138,10 +138,6 @@ const About = ({}) => {
         </Title>
         <div className="info">
           <Info>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
           </Info>
         </div>
         <div className="memberCard">
@@ -155,10 +151,7 @@ const About = ({}) => {
         </Title>
         <div className="info">
           <Info>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+           
           </Info>
         </div>
         <div className="memberCard">
