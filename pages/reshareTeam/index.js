d34c7d7c794f5fb8fa2180b2c985126d80d6b64d
Daniel Fu <danielfu123@gmail.com>
2021-11-30 14:49:39 -0800

    testing

--- pages/reshareTeam/index.js
+++ pages/reshareTeam/index.js
@@ -84,7 +84,22 @@ export default function About() {
   return (
     <AboutCont>
       <ContainerUI>
-        
+      <Title>
+          Our Developer Team
+        </Title>
+        <div className="info">
+          <Info>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          </Info>
+        </div>
+        <div className="memberCard">
+          <MemberCard name="Julian Mayes" title="Project manager &amp; Lead Developer" src="/Julian.svg" size="250px"/>
+          <MemberCard name="Michael Trinh" title="Project manager &amp; Front-end Developer" src="/Michael.svg"/>
+          <MemberCard/>
+        </div>
       </ContainerUI>
     </AboutCont>
   );
