09f908ca88f7dc3eaf630007fcdbe03e53573f6c
Daniel Fu <danielfu123@gmail.com>
2021-11-30 14:47:50 -0800

    testing

--- pages/reshareTeam/index.js
+++ pages/reshareTeam/index.js
@@ -1,11 +1,6 @@
-import { client } from "../../lib/sanity/client";
 import styled from "styled-components";
-import { postQuery } from "../../lib/sanity/postQuery";
-import BlockContent from "@sanity/block-content-to-react";
-import Header from "../../comps/header";
 import MemberCard from "../../comps/memberCard";
 import Form from "../../comps/form";
-
 
 const ContainerUI = styled.div`
   display: flex;
@@ -21,7 +16,7 @@ const AboutCont = styled.div`
   margin:0;
   height: 100vh;
 
-  /* .memberCard{
+  .memberCard{
     display: flex;
     width: 100vw;
     justify-content: space-around;
@@ -55,7 +50,7 @@ const AboutCont = styled.div`
     width: 45px;
     height: 45px;
     margin-left: 40px;
-  } */
+  }
 `;
 
 const Title = styled.text`
@@ -89,58 +84,9 @@ export default function About() {
   return (
     <AboutCont>
       <ContainerUI>
-        <Title>
-          Our Developer Team
-        </Title>
-        <div >
-          <Info>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          </Info>
-        </div>
-        <div >
-          <MemberCard name="Julian Mayes" title="Project manager &amp; Lead Developer" src="/Julian.svg" size="250px"/>
-          <MemberCard name="Michael Trinh" title="Project manager &amp; Front-end Developer" src="/Michael.svg"/>
-          <MemberCard/>
-        </div>
-
-        <Title>
-          Our Designer Team
-        </Title>
-        <div >
-          <Info>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          </Info>
-        </div>
-        <div >
-          <MemberCard name="Juhee Kim" title="Lead Designer &amp; UI/UX Designer" src="/Juhee.svg" color="#EE9837"/>
-          <MemberCard name="Glian Palma" title="UI/UX Designer &amp; Front-end Develper" src="/Glian.svg" color="#EE9837" size="220px"/>
-          <MemberCard name="Tiffany Tu" title="UI/UX Designer &amp; Marketer" src="/Tiffany.svg" color="#EE9837"/>
-        </div>
-
-        <SubTitle>Want to get started?</SubTitle>
-        <div >
-          <Form/>
-          <div >
-            <div>
-              Reshare helps reduce food waste in Vancouver by letting grocery stores sell excess inventory at a discounted price.
-            </div>
-            <div >
-              Want to get started?
-            </div>
-            <div >
-              <img src="/Union.svg"></img>
-              <img  src="/bc-institute-technology.png"></img>
-            </div>
-          </div>        
-        </div>
+        
       </ContainerUI>
-  </AboutCont>
+    </AboutCont>
   );
 };
 
